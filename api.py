cd8c96b4239e0f617f18928ec9622350ca255e55
Cole Boruk <kristineboruk@Macbook-Air.local>
2026-02-25 17:25:41 -0500

    added live odds to picks endpoint

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import json
 import os
-
+from odds import get_nba_odds
 app = FastAPI()
 
 app.add_middleware(
@@ -15,16 +15,23 @@ app.add_middleware(
 
 @app.get("/")
 def home():
-    return {"status": "NBA Quant Edge API is live"}
+    return {
+
+"status": "NBA Quant Edge API is live"}
 
 @app.get("/picks")
 def get_picks():
+    model_data = {}
+
     file_path = "reports/today_latest.json"
 
-    if not os.path.exists(file_path):
-        return {"error": "No picks generated yet"}
+    if os.path.exists(file_path):
+        with open(file_path, "r") as f:
+            model_data = json.load(f)
 
-    with open(file_path, "r") as f:
-        data = json.load(f)
+    odds_data = get_nba_odds()
 
-    return data
+    return {
+        "model": model_data,
+        "odds": odds_data
+    }
